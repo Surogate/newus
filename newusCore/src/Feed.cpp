4827c666b09ddc324cd1da42791c0bbcdcb5b964
ancelf <ancelf@localhost>
2011-08-06 14:47:11 +0200

    forgeting file

---
+++ newusCore/src/Feed.cpp
@@ -0,0 +1,21 @@
+
+#include "Feed.hpp"
+#include "FeedManager.hpp"
+
+Feed::Feed(FeedManager& fm, const std::string& addr) : _forge(fm), _host(), _path() {
+	parseAddr(addr);
+	std::cout << "host " << _host << std::endl;
+	std::cout << "path " << _path << std::endl;
+}
+
+void Feed::parseAddr(const std::string& addr) {
+	unsigned int index = 0;
+	while (addr[index] != 'w') index++;
+	unsigned int start = index;
+	while (addr[index] != '/') index++;
+	unsigned int end = index;
+	unsigned int pathEndIndex = addr.size() - 1;
+	while (addr[pathEndIndex] != '/') pathEndIndex--;
+	_host = addr.substr(start, end - start);
+	_path = addr.substr(end, pathEndIndex - end);
+}
